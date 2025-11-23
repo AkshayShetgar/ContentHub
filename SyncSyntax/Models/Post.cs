de0a78c983cb146b84094ed1270f20b4ea8d4564
Akshay Shetgar <akshay77shetgar@gmail.com>
2025-11-23 15:48:55 +0530

    Welcome to Content Hub...

---
+++ SyncSyntax/Models/Post.cs
@@ -0,0 +1,12 @@
+﻿namespace SyncSyntax.Models
+{
+    public class Post
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public string Author { get; set; }
+        public string ImagePath { get; set; }
+        public DateTime PublishedDate { get; set; }
+    }
+}
